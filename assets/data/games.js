c5cce1251ebf4971e6f8f2015984262d38f52d70
Dragon Heart Studios <95ffxnpb7k@privaterelay.appleid.com>
2026-06-22 14:00:12 +0200

    451236

--- assets/data/games.js
+++ assets/data/games.js
@@ -79,7 +79,7 @@ const GAMES_DATA = [
 		image: '/assets/images/wildbattle.png',
 		status: 'finished',
 		technologies: ['Gdevelopdzqd', 'GDScript'],
-		featured: true,
+		featured: false,
 		link: "https://wildbattle.page.gd/?i=2"
 	},
 	
@@ -98,6 +98,23 @@ const GAMES_DATA = [
 		technologies: ['Flutter', 'Firebase'],
 		featured: false,
 		link: "https://www.youtube.com/watch?v=Aq5WXmQQooo"
+	},
+
+	{
+		id: 'Rot Within',
+		title: { fr: 'Rot Within', en: 'Rot Within', es: 'Rot Within', de: 'Rot Within', it: 'Rot Within' },
+		shortDescription: {
+			fr: 'Vous incarnez Decker, un cuisinier criblé de dettes. Vos choix moraux modifieront votre apparence physique…',
+			en: 'You play as Decker, a cook in debt. Your moral choices will change your physical appearance...',
+			es: 'Juegas en el papel de Decker, un cocinero endeudado. Tus decisiones morales cambiarán tu aspecto físico...',
+			de: 'Du spielst Decker, einen verschuldeten Koch. Deine moralischen Entscheidungen beeinflussen dein Aussehen...',
+			it: 'Vesti i panni di Decker, un cuoco indebitato. Le tue scelte morali cambieranno il tuo aspetto fisico...'
+		},
+		image: '/assets/images/rot_within.png',
+		status: 'in-development',
+		technologies: ['Godot4', 'itch.io'],
+		featured: true,
+		link: "https://dragon-heart-studio.itch.io/rot-within"
 	}
 ];
 
